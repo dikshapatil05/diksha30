4eae5ac5060a65639453abcc7429e5b040cbc86a
dikshapatil05 <dp346443@gmail.com>
2025-06-13 15:46:27 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import AdmissionsPage from './pages/AdmissionsPage';
+import ContactPage from './pages/ContactPage';
+import CoursesPage from './pages/CoursesPage';
+
+import NotFoundPage from './pages/NotFoundPage';
+import ChatbotComponent from './components/Chaboat/ChatbotComponents';
+
+function App() {
+  return (
+    <Router>
+     
+      
+      <main style={{ padding: '1rem' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/apply" element={<AdmissionsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+           <Route path="/courses" element={<CoursesPage/>} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+      <ChatbotComponent/>
+    </Router>
+  );
+}
+
+export default App;
